09783d6bf91fa04076319b70c32dc981ff49dc6d
Aleksandra Balon <a.f.w.783@gmail.com>
2025-02-14 14:12:54 +0100

    Add devtools_options.yaml for DevTools settings and enhance main.dart with dotenv loading and logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
- 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_movies_api/app/config/log.dart';
 import 'package:flutter_movies_api/app/injection/injection_container.dart';
 
 import 'src/app.dart';
@@ -8,8 +8,15 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await dotenv.load(fileName: ".env");
+    globalLog.logInfo('Loading .env file correctly');
+  } catch (e) {
+    globalLog.logError('Loading .env file incorrectly - $e');
+  }
   configureDependencies();
-  await dotenv.load(fileName: ".env");
+
   final settingsController = SettingsController(SettingsService());
 
   await settingsController.loadSettings();
