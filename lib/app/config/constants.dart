ff8264252e6faa9d97c189f0f97a78eeddf12a06
Aleksandra Balon <a.f.w.783@gmail.com>
2025-06-23 22:10:52 +0200

    Update color constants and refine text styles in constants.dart

--- lib/app/config/constants.dart
+++ lib/app/config/constants.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 
-const bgColor = Color(0xFF111111);
+const bgColor = Color(0xFF191919);
+const secColor = Color(0xFF212121);
 const white = Color(0xFFFFFFFF);
 const dividerColor = Color(0xFF5E5E5E);
+const red = Color(0xFFDD1616);
 
 const defaultPadding = 20.0;
 const defaultPaddingDesktop = 40.0;
 
-const headlineLarge =
-    TextStyle(fontSize: 40, fontWeight: FontWeight.w500, height: 1);
+const headlineLarge = TextStyle(fontSize: 40, fontWeight: FontWeight.w500);
 const headlineMedium = TextStyle(fontSize: 16, fontWeight: FontWeight.w700);
-
-const bodyLarge = TextStyle(fontSize: 16);
 const bodyMedium = TextStyle(fontSize: 14);
 const bodySmall = TextStyle(fontSize: 12);
